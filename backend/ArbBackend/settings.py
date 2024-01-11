4b211193fb0ce9419aaf2198515066ca7aeacbe4
nickkatsios <t8200071@aueb.gr>
2024-01-11 17:29:20 +0200

    Backend: remove hardcoded database configs

--- backend/ArbBackend/settings.py
+++ backend/ArbBackend/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 
 from celery.schedules import crontab
 
+from dotenv import load_dotenv
+
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,17 +82,24 @@ WSGI_APPLICATION = "ArbBackend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+load_dotenv()
+
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Arb', 
-        'USER': 'root',  # Change this to your MySQL local username from the .env file
-        'PASSWORD': '9k"eAf|8', # Change this to your MySQL local password from the .env file
-        'HOST': 'localhost',  # or the IP address of your MySQL server
-        'PORT': '3306',      # default MySQL port
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        "PASSWORD": DB_PASSWORD,
+        "HOST": DB_HOST,
+        "PORT": DB_PORT,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
